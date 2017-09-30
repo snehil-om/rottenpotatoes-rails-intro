4dbe405f68a9b4f821e3b62e14c324f13c636e00
Snehil <snehil.0210@gmail.com>
2017-09-30 00:05:55 +0000

    part 2 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,10 +10,38 @@ class MoviesController < ApplicationController
     # will render app/views/movies/show.<extension> by default
   end
 
+  #def index
+    #@movies = Movie.all
+  #end
+  
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.ratings
+    redirect = false
+    @sort_by = params[:sort_by]
+    if (@sort_by)
+      session[:sort_by] = @sort_by
+    elsif session[:sort_by]
+      @sort_by = session[:sort_by]
+      redirect = true
+    end
+    @ratings ||= []
+    if params[:ratings]
+      @ratings = (params[:commit] == 'Refresh') ? params[:ratings].keys : params[:ratings]
+      session[:ratings] = @ratings
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      redirect = true
+    end
+    if @ratings.empty?
+      @movies = Movie.order(@sort_by)
+    else
+     @movies = Movie.where(:rating => @ratings).order(@sort_by)
+    end
+    if (redirect) 
+      redirect_to movies_path({:sort_by => @sort_by, :ratings => @ratings}) 
+    end
   end
-
+  
   def new
     # default: render 'new' template
   end
